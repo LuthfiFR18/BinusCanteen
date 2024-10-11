f495e39ef95b1201fee1ef83bd627f68f18d1e1f
LuthfiFR18 <127077444+LuthfiFR18@users.noreply.github.com>
2024-10-11 10:04:54 +0700

    fix backend 11/10

--- backend/controllers/RoleController.js
+++ backend/controllers/RoleController.js
@@ -1,16 +1,38 @@
 import User from '../models/UserModel.js';
 import Role from '../models/RoleModel.js';
 
-export const getUsersWithRoles = async (req, res) => {
+
+export const createRole = async (req, res) => {
     try {
-        const users = await User.findAll({
+        const { name } = req.body; // Ambil nama role dari request body
+        const role = await Roles.create({ name }); // Buat role baru di database
+        res.status(201).json(role); // Kirimkan response dengan data role baru
+    } catch (error) {
+        res.status(500).json({ message: error.message }); // Tangani error jika ada
+    }
+};
+
+export const getRoleName = async (req, res) => {
+    try {
+        const roles = await Roles.findAll({
             include: [{
-                model: Role,
-                attributes: ['roletype'] // Mengambil hanya atribut roletype dari Role
+                model : Roles,
+                attributes: ['name']
             }]
-        });
-        
-        res.status(200).json(users);
+        }); // Mengambil semua peran dari database
+        res.json(roles);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Mendapatkan peran berdasarkan ID
+export const getRoleById = async (req, res) => {
+    const { id } = req.params; // Ambil id dari parameter
+    try {
+        const role = await Roles.findOne({ where: { uuid: id } });
+        if (!role) return res.status(404).json({ message: "Role not found" });
+        res.json(role);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
